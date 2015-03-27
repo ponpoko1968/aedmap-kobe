96ac5292e6c2553416819b899ca6daa38c2be289
越智 修司 <ponpoko1968@gmail.com>
2015-03-27 13:47:47 +0900

    CLLocationManagerの謎の挙動がどうしても解決できないので、プロジェクトを作り直す

--- AEDMap/AMMapViewController.m
+++ AEDMap/AMMapViewController.m
@@ -1,21 +1,27 @@
 //
-//  SecondViewController.m
+//  FirstViewController.m
 //  AEDMap
 //
 //  Created by 越智 修司 on 2015/03/27.
 //  Copyright (c) 2015年 越智 修司. All rights reserved.
 //
 
-#import "SecondViewController.h"
+#import "AMMapViewController.h"
 
-@interface SecondViewController ()
+@interface AMMapViewController ()
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
 
-@implementation SecondViewController
-
+@implementation AMMapViewController
+{
+  CLLocationManager* _locationManager;
+}
 - (void)viewDidLoad {
   [super viewDidLoad];
+  _locationManager = [[CLLocationManager alloc] init];
+  [_locationManager requestWhenInUseAuthorization];
+  self.mapView.showsUserLocation=YES;
   // Do any additional setup after loading the view, typically from a nib.
 }
 
