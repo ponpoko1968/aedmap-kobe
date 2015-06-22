4e2698d55a4cdb775171109dca160a71c4c660ed
Shuji OCHI(air) <ponpoko1968@gmail.com>
2015-06-22 15:26:34 +0900

    markdown組み込む

---
+++ AEDMap/AMAboutThisAppViewController.h
@@ -0,0 +1,13 @@
+//
+//  AMAboutThisAppViewController.h
+//  AEDMap
+//
+//  Created by 越智 修司 on 2015/06/22.
+//  Copyright (c) 2015年 越智 修司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMAboutThisAppViewController : UIViewController
+
+@end
--- Podfile
+++ Podfile
@@ -6,3 +6,4 @@ pod 'GVUserDefaults', '~> 0.9'
 pod 'SVProgressHUD', '~> 1.1'
 pod 'ADClusterMapView', '~> 1.0.2'
 pod "CCMPopup"
+pod 'UIWebView-Markdown' , :git => 'https://github.com/Huddle/UIWebView-Markdown.git'
